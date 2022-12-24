395d8ec7cf597c18c807097dc8b436b9f7d9a218
muratyildirimm <muratyildirimm495@gmail.com>
2022-12-24 22:28:33 +0300

    share post pageyi revize ettim

---
+++ app/src/main/java/com/example/socialwedding/activities/SharePostActivity.java
@@ -0,0 +1,16 @@
+package com.example.socialwedding.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.socialwedding.R;
+
+public class SharePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_post);
+    }
+}
