e2c52830c3745f284fe8b787e862880b9e2d9f16
muratyildirimm <muratyildirimm495@gmail.com>
2022-12-24 16:54:18 +0300

    simple progress bar is done

--- app/src/main/java/com/example/socialwedding/MainActivity.java
+++ app/src/main/java/com/example/socialwedding/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.socialwedding.activities.HomeActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailEditText;
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     String dummyEmail = "abc@gmail.com";
     String dummyPassword = "123456";
     String loginSuccessCode = "login-success";
+    ProgressBar pb;
+    int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,23 +32,51 @@ public class MainActivity extends AppCompatActivity {
         emailEditText =  (EditText) findViewById(R.id.id_email);
         passwordEditText = (EditText) findViewById(R.id.id_password);
         button = (Button) findViewById(R.id.login);
+        pb=(ProgressBar)findViewById(R.id.main_activity_progress_bar);
+
         String loginMessage;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)  {
+                progress();
+                pb.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this,
+                                HomeActivity.class);
+                        MainActivity.this.startActivity(intent);
 
-                Intent intent = new Intent(MainActivity.this,
-                        HomeActivity.class);
-                MainActivity.this.startActivity(intent);
-                if(emailEditText.getText().toString().equals(dummyEmail) && dummyPassword.equals(passwordEditText.getText().toString())){
-                    showToastMessage(LOGINSTATUS.LOGIN_SUCCESS);
+                        if(emailEditText.getText().toString().equals(dummyEmail) && dummyPassword.equals(passwordEditText.getText().toString())){
+                            showToastMessage(LOGINSTATUS.LOGIN_SUCCESS);
 
 
-                }else {
-                    showToastMessage(LOGINSTATUS.LOGIN_FAILED);
+                        }else {
+                            showToastMessage(LOGINSTATUS.LOGIN_FAILED);
+                        }
+                    }
+
+                }, 5000);
+
+            }
+
+        });
+    }
+
+   public void progress()   {
+
+        final Timer t=new Timer();
+        TimerTask tt=new TimerTask() {
+            @Override
+            public void run() {
+                counter+=2;
+                pb.setProgress(counter);
+                if(counter== 100){
+                    t.cancel();
                 }
             }
-        });
+        };
+        t.schedule(tt,0,100);
     }
      void showToastMessage(LOGINSTATUS loginstatus) {
        switch (loginstatus){
