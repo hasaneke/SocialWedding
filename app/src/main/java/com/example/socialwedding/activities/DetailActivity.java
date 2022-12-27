c9df2666f92afe81c60f86d4cbcaeef2ec7824bc
muratyildirimm <muratyildirimm495@gmail.com>
2022-12-27 15:26:15 +0300

    direkt pushladım kanziii

--- app/src/main/java/com/example/socialwedding/activities/DetailActivity.java
+++ app/src/main/java/com/example/socialwedding/activities/DetailActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.Random;
 
 import com.example.socialwedding.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    ImageView pp;
+    Random r;
+    String[] nameList={"Shane","Billy","Ariel","Jess","Kim","Alexis","Avery","Toby","Stacy","Riley"};
+    String[] commentList={};
+    int[] photoList={R.drawable.person1,R.drawable.person2,R.drawable.person3,R.drawable.person4,
+            R.drawable.person5,R.drawable.person6,R.drawable.person7,R.drawable.person8,
+            R.drawable.person9,R.drawable.person10};
     private String gelenCoupleNames;
     private int[] arr;
-    private TextView tv;
+    private TextView tv,comment1;
     private TextView descriptionTextView;
     String coupleName;
     String description;
@@ -22,6 +31,13 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         tv= (TextView)findViewById(R.id.tv);
+        pp=(ImageView)findViewById(R.id.profile_photo);
+        pp.setBackgroundResource(photoList[8]);
+        r=new Random();
+
+        comment1=(TextView)findViewById(R.id.name_text_view);
+
+        comment1.setText("tebriklerrr supersniz<<");
         Intent intent=getIntent();
         coupleName=intent.getStringExtra("coupleNames");
         tv.setText(coupleName);
